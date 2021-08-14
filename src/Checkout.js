95a25c260c457851bb2ca194021e6252f862bb33
Nirajan Mahara <mnirajan9@gmail.com>
2021-08-14 15:57:29 +0545

    Add `Subtotal` component `template and styles`

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Checkout.css';
+import Subtotal from './Subtotal';
 
 function Checkout() {
   return (
@@ -21,7 +22,7 @@ function Checkout() {
       </div>
 
       <div className='checkout__right'>
-        <h1>The SUBTOTAL will go here</h1>
+        <Subtotal />
       </div>
     </div>
   );
