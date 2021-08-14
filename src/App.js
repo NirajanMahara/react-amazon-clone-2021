6a1fed8deede108dac6048ec65c8ce97431d7683
Nirajan Mahara <mnirajan9@gmail.com>
2021-08-14 15:33:19 +0545

    Add `Checkout` Component template `js & css` to `App.js`

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Checkout from './Checkout';
 
 function App() {
   // BEM naming convention
@@ -12,7 +13,7 @@ function App() {
         <Switch>
           <Route path='/checkout'>
             <Header />
-            <h1>I'm a CHECKOUT Page 🚀</h1>
+            <Checkout />
           </Route>
 
           <Route path='/'>
