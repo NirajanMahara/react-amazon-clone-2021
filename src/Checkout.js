2f49562d946554646df487e79a8c8c1c7ea22b0b
Nirajan Mahara <mnirajan9@gmail.com>
2021-08-14 22:16:47 +0545

    🛒🎁 Add `CheckoutProduct` component using the `{ basket } = useStateValue()` & `basket.map(item => passing props)` to `CheckoutProduct` for render in `Checkout` component

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './Checkout.css';
 import Subtotal from './Subtotal';
+import { useStateValue } from './StateProvider';
+import CheckoutProduct from './CheckoutProduct';
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className='checkout'>
       <div className='checkout__left'>
@@ -15,9 +19,15 @@ function Checkout() {
         <div>
           <h2 className='checkout__title'>Your Shopping Basket</h2>
 
-          {/* <CheckoutProduct /> */}
-          {/* <CheckoutProduct /> */}
-          {/* <CheckoutProduct /> */}
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              image={item.image}
+              title={item.title}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
 
