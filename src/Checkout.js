746206b02b4bfa73304fa412f98482269d94c5de
Nirajan Mahara <mnirajan9@gmail.com>
2021-08-14 15:45:01 +0545

    Add `checkout__left` `checkout__right` div `templates & styles`💳

--- src/Checkout.js
+++ src/Checkout.js
@@ -4,7 +4,25 @@ import './Checkout.css';
 function Checkout() {
   return (
     <div className='checkout'>
-      <h1>I'm a CHECKOUT Page 🚀</h1>
+      <div className='checkout__left'>
+        <img
+          className='checkout__ad'
+          src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg'
+          alt=''
+        />
+
+        <div>
+          <h2 className='checkout__title'>Your Shopping Basket</h2>
+
+          {/* <CheckoutProduct /> */}
+          {/* <CheckoutProduct /> */}
+          {/* <CheckoutProduct /> */}
+        </div>
+      </div>
+
+      <div className='checkout__right'>
+        <h1>The SUBTOTAL will go here</h1>
+      </div>
     </div>
   );
 }
