982e03ea2ecb6a0a07e83ab9b109443d28a3052e
Nirajan Mahara <mnirajan9@gmail.com>
2021-08-15 13:05:09 +0545

    Grab `firebaseConfig` of Web App to `firebase.js` ⚙

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAdkf8iQgkKHRACwDQj-p0wdtl-lUR02wM',
+  authDomain: 'react-amzn-clone-2021.firebaseapp.com',
+  projectId: 'react-amzn-clone-2021',
+  storageBucket: 'react-amzn-clone-2021.appspot.com',
+  messagingSenderId: '198482799631',
+  appId: '1:198482799631:web:ac938deaa71ac8f52ec77a',
+  measurementId: 'G-ZHC6JJTD9X',
+};
