b27adcc933c514acdd957dac312fa3416aa3a96a
Nirajan Mahara <mnirajan9@gmail.com>
2021-08-15 12:12:51 +0545

    Track the `<input />` fields ⌨ - set piece of `state variable` using `initial state useState('')` hook 💫 - onChange of `event` of `<input />` fields `setEmail(event.target.value)`

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div className='login'>
       <Link to='/'>
@@ -17,10 +20,18 @@ function Login() {
 
         <form>
           <h5>Email</h5>
-          <input type='text' />
+          <input
+            type='text'
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type='password' />
+          <input
+            type='password'
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
 
           <button className='login__signInButton'>Sign-In</button>
         </form>
