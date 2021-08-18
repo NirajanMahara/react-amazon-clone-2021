baad82eb519df0cc3bfcc5ec7a38eef2332ea0b4
Nirajan Mahara <mnirajan9@gmail.com>
2021-08-18 14:25:01 +0545

    💳 Build Payment/Checkout page - using also `useStateValue` for `{user?.email}` & `basket` && `<CheckoutProduct />` component for items

--- src/Payment.js
+++ src/Payment.js
@@ -1,15 +1,58 @@
 import React from 'react';
 import './Payment.css';
+import { useStateValue } from './StateProvider';
+import { Link } from 'react-router-dom';
+import CheckoutProduct from './CheckoutProduct';
 
 function Payment() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
   return (
     <div className='payment'>
       <div className='payment__container'>
+        <h1>
+          Checkout (<Link to='/checkout'>{basket?.length} items</Link>)
+        </h1>
+
         {/* payment__section - Delivery Address */}
+        <div className='payment__section'>
+          <div className='payment__title'>
+            <h3>Delivery Address</h3>
+          </div>
+          <div className='payment__address'>
+            <p>{user?.email}</p>
+            <p>React, JavaScript 1997</p>
+            <p>Silicon Valley, CA</p>
+          </div>
+        </div>
 
         {/* payment__section - Review Items */}
+        <div className='payment__section'>
+          <div className='payment__title'>
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className='payment__items'>
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
 
         {/* payment__section - Payment Method */}
+        <div className='payment__section'>
+          <div className='payment__title'>
+            <h3>Payment Method</h3>
+          </div>
+          <div className='payment__details'>
+            {/* Stripe magic */}
+          </div>
+        </div>
       </div>
     </div>
   );
